b4758a62490d0ec75b3851436d3df52959f0a3a0
vidasaras <vidasara555@gmail.com>
2025-05-31 19:11:28 +0530

    v1

---
+++ bfr.h
@@ -0,0 +1,130 @@
+#ifndef BFR_H
+#define BFR_H
+
+#include <stdlib.h>
+
+struct Font_config {
+  int ch_w,
+      ch_h,
+      margin_x,
+      margin_y,
+      ch_sp,
+      line_sp,
+      ch_line,
+      nm_line,
+      nm_ch,
+      scale;
+};
+
+int bfr_init(struct Font_config conf);
+// Initializes the font system with the given configuration.
+void bfr_destroy();
+// Frees memory used by the font system.
+int load_font_image(SDL_Renderer* renderer, char* file, SDL_Texture** font_texture);
+// Loads the font image
+void load_ascii(char* file);
+// Loads ASCII codes from a file into memory.
+void render_ch(SDL_Renderer* renderer, SDL_Texture* font_texture, char ch, int ch_in_line_i, int line_i);
+// Renders a single character at the specified grid position.
+void render_line(SDL_Renderer* renderer, SDL_Texture* font_texture, char* ch, int line_i);
+// Renders a line of text at the given line index.
+void render_lines(SDL_Renderer* renderer, SDL_Texture* font_texture, char* ch);
+// Renders multiple lines of text separated by '\n'.
+
+#ifdef BFR_IMPLEMENTATION
+
+int* ascii = NULL;
+struct Font_config* BFRfont_config = NULL;
+
+int bfr_init(struct Font_config conf){
+
+  BFRfont_config = malloc(sizeof(struct Font_config));
+  *BFRfont_config = conf;
+
+ ascii = malloc(sizeof(int) * BFRfont_config->nm_ch);
+
+  if (!BFRfont_config || !ascii) return 1;
+  return 0;
+}
+
+void bfr_destroy(){
+  free(ascii);
+  free(BFRfont_config);
+}
+
+int load_font_image(SDL_Renderer* renderer, char* file, SDL_Texture** font_texture){
+  *font_texture =  IMG_LoadTexture(renderer, file);
+  if (*font_texture == NULL) return 1;
+
+  SDL_SetTextureBlendMode(*font_texture, SDL_BLENDMODE_BLEND);
+  return 0;
+}
+
+void load_ascii(char* file){
+  FILE* file_ptr;
+  file_ptr = fopen(file, "r");
+
+  char line[20];
+  int i = 0;
+  while (fgets(line, 20, file_ptr) != NULL) {
+    if (i >= BFRfont_config->nm_ch) break;
+    ascii[i] = atoi(line);
+    i++;
+  }
+
+  fclose(file_ptr);
+}
+
+void render_ch(SDL_Renderer* renderer, SDL_Texture* font_texture, char ch, int ch_in_line_i, int line_i){
+  int i = 0;
+  while (i < BFRfont_config->nm_ch){
+    if (ch == ascii[i]) break;
+    i++;
+  }
+  if (i == BFRfont_config->nm_ch) return;
+
+  int line_num = i / BFRfont_config->ch_line;
+  int ch_in_line_num = i % BFRfont_config->ch_line;
+
+  int x = BFRfont_config->margin_x + ch_in_line_num * BFRfont_config->ch_sp + ch_in_line_num * BFRfont_config->ch_w;
+  int y = BFRfont_config->margin_y + line_num * BFRfont_config->line_sp + line_num * BFRfont_config->ch_h;
+
+  SDL_Rect r_src = {x, y, BFRfont_config->ch_w, BFRfont_config->ch_h};
+  SDL_Rect r_dst = {
+    BFRfont_config->ch_w * ch_in_line_i + ch_in_line_i * 3,
+    BFRfont_config->ch_h * line_i + 8 * line_i,
+    BFRfont_config->ch_w * BFRfont_config->scale,
+    BFRfont_config->ch_h * BFRfont_config->scale
+  };
+
+  SDL_RenderCopy(renderer, font_texture, &r_src, &r_dst);
+}
+
+void render_line(SDL_Renderer* renderer, SDL_Texture* font_texture, char* ch, int line_i){
+  int i = 0;
+  while (ch[i] != '\0'){
+    render_ch(renderer, font_texture, ch[i], (int)i+1, line_i);
+    i++;
+  }
+}
+
+void render_lines(SDL_Renderer* renderer, SDL_Texture* font_texture, char* ch){
+  int i = 0;
+  int ch_i = 1;
+  int line_i = 1;
+  while (ch[i] != '\0'){
+    if (ch[i] == '\n'){
+      line_i++;
+      ch_i = 1;
+      i++;
+      continue;
+    }
+    render_ch(renderer, font_texture, ch[i], ch_i, line_i);
+    ch_i++;
+    i++;
+  }
+}
+
+#endif
+
+#endif
---
+++ example.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <unistd.h>
+#define BFR_IMPLEMENTATION
+#include "bfr.h"
+
+struct Font_config font_config = {
+  .ch_w = 40,
+  .ch_h = 72,
+  .margin_x = 48,
+  .margin_y = 48,
+  .ch_sp = 8,
+  .line_sp = 8,
+  .ch_line = 13,
+  .nm_line = 7,
+  .nm_ch = 88,
+  .scale = 1
+};
+
+int main(){
+  if (SDL_Init(SDL_INIT_VIDEO) < 0){
+    return 1;
+  }
+
+  SDL_Window *win =  SDL_CreateWindow("Bitmap Font Renderer",
+      SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800,
+      600, 0);
+
+  SDL_Renderer *renderer =  SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
+
+  if (bfr_init(font_config) != 0) printf("Bitmap font init failed!\n");
+
+  SDL_Texture* font_texture = NULL;
+  load_font_image(renderer, "font.png", &font_texture);
+
+  load_ascii("input");
+
+  render_lines(renderer, font_texture, "Hello, World!");
+
+  SDL_RenderPresent(renderer);
+  sleep(3);
+
+  bfr_destroy();
+  SDL_DestroyTexture(font_texture);
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(win);
+  SDL_Quit();
+
+  return 0;
+}
